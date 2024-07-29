15cfd036ad763bc76829f391690696302b55dd64
thespekky <bandras123456@gmail.com>
2024-07-29 18:19:11 +0200

    frontend backend létrehozása,backend kapcsolat elkészítése az adatbázissal

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+app.use("/", require("./Routes/routes"));
+
+const uri =
+  "mongodb+srv://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@cluster0.vfvg2lr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+console.log(uri);
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
