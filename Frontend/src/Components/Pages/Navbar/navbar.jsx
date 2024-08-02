a0c85d716c044249863a80371108d92a800c0451
thespekky <bandras123456@gmail.com>
2024-08-02 22:58:18 +0200

    navbar működésének elkészítése

---
+++ Frontend/src/Components/Pages/Navbar/navbar.jsx
@@ -0,0 +1,69 @@
+import { Link, useResolvedPath, useMatch } from "react-router-dom";
+import { useAuth } from "../../AuthContext/AuthContext";
+import { useEffect } from "react";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+export default function Navbar() {
+  const { isLoggedIn, logout } = useAuth();
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand">
+            Unicomp
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <CostumeLink to={"/"}>Könyvek</CostumeLink>
+              {isLoggedIn ? (
+                <CostumeLink to={"/felhasznalo"}>Felhasználó</CostumeLink>
+              ) : (
+                <></>
+              )}
+            </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              {isLoggedIn ? (
+                <CostumeLink to={"/belepes"} className="btn btn-primary">
+                  Belépés
+                </CostumeLink>
+              ) : (
+                <CostumeLink
+                  to={"/kilepes"}
+                  onlcick={logout}
+                  className="btn btn-danger"
+                >
+                  Kilépés
+                </CostumeLink>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+function CostumeLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "nav-item active" : "nav-item notactive"}>
+      <Link className="nav-link" aria-current="page" to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./Css/main.css";
 import "./Components/AuthContext/AuthContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
